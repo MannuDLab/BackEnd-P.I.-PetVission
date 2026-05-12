f9439e0448adbba067db39cb5fe79aa13b01d978
Cristian-DH <cristian.alexis.d.h@gmail.com>
2026-05-12 18:36:04 -0400

    Implement cancel and reschedule appointment methods
    Se agregaron los códigos para implementar métodos de cancelar y reprogramar citas

--- src/main/java/org/example/petvission/cita/service/CitaService.java
+++ src/main/java/org/example/petvission/cita/service/CitaService.java
@@ -1,4 +1,64 @@
 package org.example.petvission.cita.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.petvission.cita.dto.ReprogramarCitaDto;
+import org.example.petvission.cita.model.Cita;
+import org.example.petvission.cita.model.EstadoCita;
+import org.example.petvission.cita.repository.CitaRepository;
+import org.example.petvission.shared.exception.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CitaService {
+
+    private final CitaRepository citaRepository;
+
+    /*
+     * CANCELAR CITA
+     */
+    public Cita cancelarCita(Long idCita) {
+
+        Cita cita = citaRepository.findById(idCita)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Cita no encontrada"
+                        )
+                );
+
+        /*
+         * VALIDAR QUE NO ESTÉ CANCELADA
+         */
+        if (cita.getEstado() == EstadoCita.CANCELADA) {
+            throw new IllegalStateException(
+                    "La cita ya está cancelada"
+            );
+        }
+
+        cita.setEstado(EstadoCita.CANCELADA);
+
+        return citaRepository.save(cita);
+    }
+
+    /*
+     * REPROGRAMAR CITA
+     */
+    public Cita reprogramarCita(
+            Long idCita,
+            ReprogramarCitaDto dto
+    ) {
+
+        Cita cita = citaRepository.findById(idCita)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Cita no encontrada"
+                        )
+                );
+
+        /*
+         * VALIDAR QUE NO ESTÉ CANCELADA
+         */
+        if (cita.getEstado() == EstadoCita.CANCELADA) {
+            throw new IllegalStateException(
+                    "No se puede reprogramar una cita cancelada"
 }
